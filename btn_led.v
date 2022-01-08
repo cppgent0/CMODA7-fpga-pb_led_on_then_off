8fd97f804a01112b4fe2dc51919cce9ee6747600
cppgent0 <cppgent0@gmail.com>
2022-01-08 09:02:55 -0800

    more clean up

--- btn_led.v
+++ btn_led.v
@@ -1,24 +1,25 @@
 `timescale 1ns / 1ps
 
 module toggle_led (
-    input sysclk,     // system clock
-    input [1:0]btn,   // BTN1
-    output [1:0]led   // LED1
-);          
+    input sysclk,   // system clock
+    input [1:0]btn, // BTN1
+    output [1:0]led // LED1
+);
 
-    reg btn_aligned;  // button press aligned with system clock
-    reg state;        // the current state of the LED 
-    reg toggle_flag;  // indicates whether to toggle the state 
+    reg btn_aligned; // button press aligned with system clock
+    reg led_state;   // the current state of the LED 
+    reg toggle_flag; // indicates whether to toggle the state 
 
     always @ (posedge sysclk) begin
         btn_aligned <= btn[0];
+        // check if the btn has changed state
         toggle_flag <= btn[0] & ~btn_aligned;
         if (toggle_flag) begin
-            // toggle the state
-            state <= ~state;
+            // button has changed state, so toggle the led state
+            led_state <= ~led_state;
         end
     end
-    
+
     // set led to the internal state
-    assign led[0] = state;
+    assign led[0] = led_state;
 endmodule 
